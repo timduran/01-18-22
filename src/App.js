80263e203f697f3b38fc91280e3e458bfcadff07
Quinton Fults <quinton.fults.work@gmail.com>
2022-01-18 14:05:20 -0800

    code

--- src/App.js
+++ src/App.js
@@ -1,69 +1,31 @@
-import { useState } from "react"
+import { useEffect, useState } from 'react'
+import axios from 'axios'
 
 const App = () => {
 
-  const [userState, setUserState] = useState({
-    name: '',
-    email: '',
-    password: '',
-    user: {}
+  const [pokeState, setPokeState] = useState({
+    pokemon: {}
   })
 
-  const handleInputChange = ({ target: { name, value } }) => {
-    setUserState({ ...userState, [name]: value })
-  }
-
-  const handleFormSubmit = event => {
-    event.preventDefault()
-    setUserState({
-      ...userState,
-      user: {
-        name: userState.name,
-        email: userState.email,
-        password: userState.password
-      },
-      name: '',
-      email: '',
-      password: ''
-    })
-  }
+  useEffect(() => {
+    axios.get('https://pokeapi.co/api/v2/pokemon/ditto')
+      .then(({ data: pokemon }) => {
+        console.log(pokemon)
+        setPokeState({ ...pokeState, pokemon })
+      })
+      .catch(err => console.error(err))
+  }, [])
 
   return (
     <>
-      <form>
-        <p>
-          <label htmlFor="name">name</label>
-          <input
-            type="text"
-            name="name"
-            value={userState.name}
-            onChange={handleInputChange} />
-        </p>
-        <p>
-          <label htmlFor="email">email</label>
-          <input
-            type="email"
-            name="email"
-            value={userState.email}
-            onChange={handleInputChange} />
-        </p>
-        <p>
-          <label htmlFor="password">password</label>
-          <input
-            type="password"
-            name="password"
-            value={userState.password}
-            onChange={handleInputChange} />
-        </p>
-        <p>
-          <button onClick={handleFormSubmit}>Submit</button>
-        </p>
-      </form>
-      <div>
-        <h1>{userState.user.name}</h1>
-        <h1>{userState.user.email}</h1>
-        <h1>{userState.user.password}</h1>
-      </div>
+      {
+        pokeState.pokemon.name ? (
+          <>
+            <h1>{pokeState.pokemon.name}</h1>
+            <h1>{pokeState.pokemon.species.name}</h1>
+          </>
+        ) : null
+      }
     </>
   )
 }
