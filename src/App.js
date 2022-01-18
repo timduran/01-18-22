5bba94aee6b043af4bba93499cc5850c628ad1e0
Quinton Fults <quinton.fults.work@gmail.com>
2022-01-18 11:30:57 -0800

    code

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
-const names = ['John Doe', 'Jane Doe', 'Jack Doe', 'James Doe', 'Jared Doe']
+import { useState } from 'react'
+
+// const names = ['John Doe', 'Jane Doe', 'Jack Doe', 'James Doe', 'Jared Doe']
 
 const App = () => {
+
+  const [countState, setCountState] = useState({
+    count: 0
+  })
+
+  const handleIncrementCount = () => {
+    setCountState({ count: countState.count + 1 })
+  }
+
   return (
-    <ul>
+    <>
+    {/* <ul>
       {names.map(name => <li>{name}</li>)}
-    </ul>
+    </ul> */}
+    <h1>Count: {countState.count}</h1>
+    <button onClick={handleIncrementCount}>Click Me</button>
+    </>
   )
 }
 
